b526456640fe2e6d80c944da8165f36a19659a23
sai1004 <saikiran.bavandla@gmail.com>
2019-09-13 19:49:12 +0530

    commit

--- src/app/components/chat-box/chat-box.component.ts
+++ src/app/components/chat-box/chat-box.component.ts
@@ -11,10 +11,106 @@ export class ChatBoxComponent implements OnInit {
     { 
       id: 1,
       userName: 'sai', 
-      message: ' somthing ',
+      message: ' im sai ',
       img: '../../../assets/avatar2.png'
       
     },
+    { 
+      id: 2,
+      userName: 'sam', 
+      message: ' i"m sam  ',
+      img: '../../../assets/avatar2.png'
+
+    }, { 
+      id: 3,
+      userName: 'jake', 
+      message: ' i"m jake ',
+      img: '../../../assets/avatar2.png'
+
+    }, { 
+      id: 4,
+      userName: 'ram', 
+      message: ' somthing ',
+      img: '../../../assets/avatar2.png'
+
+    }
+    // ,
+    // { 
+    //   id: 2,
+    //   userName: 'chris', 
+    //   message: ' somthing ',
+    //   img: '../../../assets/avatar2.png'
+
+    // }, { 
+    //   id: 3,
+    //   userName: 'lark', 
+    //   message: ' somthing ',
+    //   img: '../../../assets/avatar2.png'
+
+    // }, { 
+    //   id: 4,
+    //   userName: 'lin', 
+    //   message: ' somthing ',
+    //   img: '../../../assets/avatar2.png'
+
+    // },
+    // { 
+    //   id: 2,
+    //   userName: 'brad', 
+    //   message: ' somthing ',
+    //   img: '../../../assets/avatar2.png'
+
+    // }, { 
+    //   id: 3,
+    //   userName: 'jason', 
+    //   message: ' somthing ',
+    //   img: '../../../assets/avatar2.png'
+
+    // }, { 
+    //   id: 4,
+    //   userName: 'hammer', 
+    //   message: ' somthing ',
+    //   img: '../../../assets/avatar2.png'
+
+    // },
+    // { 
+    //   id: 2,
+    //   userName: 'sevlit', 
+    //   message: ' somthing ',
+    //   img: '../../../assets/avatar2.png'
+
+    // }, { 
+    //   id: 3,
+    //   userName: 'roy', 
+    //   message: ' somthing ',
+    //   img: '../../../assets/avatar2.png'
+
+    // }, { 
+    //   id: 4,
+    //   userName: 'Rathor', 
+    //   message: ' somthing ',
+    //   img: '../../../assets/avatar2.png'
+
+    // },
+    // { 
+    //   id: 2,
+    //   userName: 'emrin', 
+    //   message: ' somthing ',
+    //   img: '../../../assets/avatar2.png'
+
+    // }, { 
+    //   id: 3,
+    //   userName: 'jospeh', 
+    //   message: ' somthing ',
+    //   img: '../../../assets/avatar2.png'
+
+    // }, { 
+    //   id: 4,
+    //   userName: 'dildigo', 
+    //   message: ' somthing ',
+    //   img: '../../../assets/avatar2.png'
+
+    // }
   ]
   constructor() { }
 
